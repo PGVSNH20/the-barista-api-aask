ae6a5017f900a5849812f65764744173d23e206c
anton thunman jonsson <jonssonanton@hotmail.com>
2021-03-02 13:26:41 +0000

    Update Beverage.cs
    Tabify

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -2,19 +2,19 @@ using System.Collections.Generic;
 
 public interface IBeverage{
 	List<string> Ingredients { get; }
-    string CupType { get; }
+	string CupType { get; }
 }
 
 class Espresso : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
+	public List<string> Ingredients => throw new System.NotImplementedException();
 
-    public string CupType => throw new System.NotImplementedException();
+	public string CupType => throw new System.NotImplementedException();
 }
 
 class Latte : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
+	public List<string> Ingredients => throw new System.NotImplementedException();
 
-    public string CupType => throw new System.NotImplementedException();
+	public string CupType => throw new System.NotImplementedException();
 }
