1670f346427c417e769318b71ba746ac790f108b
Sanna Edström <70255968+Cimeiesh@users.noreply.github.com>
2021-03-02 15:31:02 +0100

    Update Beverage.cs
    Fixat ing och cup till kaffetyperna

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -19,47 +19,44 @@ class Bean
 }
 
 class Espresso : IBeverage
-
 {
-
-	public List<string> Ingredients => { "Espresso"};
+	public List<string> Ingredients { get; } = new List<string> { "Espresso" };
 		
-
-	public string CupType => "Small";
+	public string CupType { get; } = "Small";
 }
 
 class Latte : IBeverage
 {
-	public List<string> Ingredients => throw new System.NotImplementedException();
+	public List<string> Ingredients { get; } = new List<string> { "Espresso", "Milk" };
 
-	public string CupType => throw new System.NotImplementedException();
+	public string CupType { get; } = "Large";
 }
 
 class Cappuccino : IBeverage
 {
-	public List<string> Ingredients => throw new System.NotImplementedException();
+	public List<string> Ingredients { get; } = new List<string> { "Espresso", "Milk", "Milk foam" };
 
-	public string CupType => throw new System.NotImplementedException();
+	public string CupType { get; } = "Medium";
 }
 
 class Americano : IBeverage
 {
-	public List<string> Ingredients => throw new System.NotImplementedException();
+	public List<string> Ingredients { get; } = new List<string> { "Espresso", "Water"};
 
-	public string CupType => throw new System.NotImplementedException();
+	public string CupType { get; } = "Medium";
 }
 
 class Mocha : IBeverage
 {
-	public List<string> Ingredients => throw new System.NotImplementedException();
+	public List<string> Ingredients { get; } = new List<string> { "Espresso", "Chocolate syrup", "Milk" };
 
-	public string CupType => throw new System.NotImplementedException();
+	public string CupType { get; } = "Medium";
 }
 class Macchiato : IBeverage
 {
-	public List<string> Ingredients => throw new System.NotImplementedException();
+	public List<string> Ingredients { get; } = new List<string> { "Espresso", "Milk Foam" };
 
-	public string CupType => throw new System.NotImplementedException();
+	public string CupType { get; } = "Large";
 }
 
 class CustomBeverage : IBeverage
