919636747cb73d6988fee7cf5579f4d9581e5d1d
Kire <70213370+kireyperez@users.noreply.github.com>
2021-03-04 11:35:40 +0100

    Update
    implementerad add metoder,tobeverage

--- BaristaApi/EspressoMachine.cs
+++ BaristaApi/EspressoMachine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,53 @@ namespace BaristaApi
 {
     class EspressoMachine : IEspressoMachine
     {
+        public List<Ingredient> Ingredients { get; } = new List<Ingredient>();
+
+        public IEspressoMachine AddBeans()
+        {
+
+            return this;
+        }
+
+        public IEspressoMachine AddChocolateSyrup()
+        {
+            Ingredients.Add(new Ingredient() { Name = "Chocolate Syrup" });
+            return this;
+        }
+
+        public IEspressoMachine AddMilk()
+        {
+            Ingredients.Add(new Ingredient() { Name = "Milk" });
+            return this;
+        }
+
+        public IEspressoMachine AddMilkFoam()
+        {
+            Ingredients.Add(new Ingredient() { Name = "Milk Foam" });
+            return this;
+        }
+
+        public IEspressoMachine AddWater(int v)
+        {
+            Ingredients.Add(new Ingredient() { Name = "Water" });
+            return this;
+        }
+
         public IBeverage ToBeverage()
         {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type[] types = assembly.GetTypes();
             
+            foreach(var type in types)
+            {
+                if (type.GetInterfaces().Contains(typeof(IBeverage)))
+                {
+
+                }
+            }
+
+
+
         }
     }
 }
--- BaristaApi/IEspressoMachine.cs
+++ BaristaApi/IEspressoMachine.cs
@@ -1,7 +1,15 @@
-﻿namespace BaristaApi
+﻿using System.Collections.Generic;
+
+namespace BaristaApi
 {
     internal interface IEspressoMachine
     {
-
+        List<Ingredient>Ingredients { get; }
+        IEspressoMachine AddWater(int v);
+        IEspressoMachine AddBeans();
+        IEspressoMachine AddMilk();
+        IEspressoMachine AddMilkFoam();
+        IEspressoMachine AddChocolateSyrup();
+        IBeverage ToBeverage();
     }
 }
